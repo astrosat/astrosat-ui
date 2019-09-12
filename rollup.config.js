12297f14f96a6318e3ae01e1294cd8faeafad3c3
Mark Small <mark.small@astrosat.space>
2019-09-12 23:08:03 +0100

    feat(frontend): Add project config
    Added config to set:
    
    * Node version
    * babel
    * build via rollup
    * flow
    * yarn scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,22 @@
 // Rollup plugins
 import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
 
 import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.browser, format: 'umd', name: 'mapboxUtils' },
+    { file: pkg.browser, format: 'umd', name: 'astrosatui' },
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'es' }
   ],
-  sourceMap: 'inline',
+  external: ['react', 'react-dom', 'prop-types'],
   plugins: [
+    resolve(),
     postcss({
-      extensions: ['.css']
+      modules: true
     }),
     babel({
       exclude: 'node_modules/**'
