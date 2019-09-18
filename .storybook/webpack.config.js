32a327bb63d4de6282d03ed693c5afcf28d351ea
Mark Small <mark.small@astrosat.space>
2019-09-18 14:47:17 +0100

    feat(frontend): Theme components
    Wrapped components in div that will contain the theme. This is
    then applied to each component.
    
    Added webpack config to load svg images.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -17,7 +17,17 @@ module.exports = {
       },
       {
         test: /\.svg$/,
-        loader: 'svg-react-loader'
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true // true outputs JSX tags
+            }
+          }
+        ]
       }
     ]
   }
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,6 +4,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withKnobs, select } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 import { Welcome } from '@storybook/react/demo';
 
 import Button from '../src/buttons/button.component';
@@ -11,23 +12,46 @@ import CloseButton from '../src/buttons/close-button.component';
 import InfoButton from '../src/buttons/info-button.component';
 import LoadMask from '../src/load-mask/load-mask.component';
 
+import styles from '../src/index.module.css';
+
+const themes = { Dark: styles.dark, Light: styles.light };
+
 storiesOf('Welcome', module).add('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
 ));
 
 storiesOf('Buttons', module)
+  // .addDecorator(withInfo)
   .addDecorator(withKnobs)
   .add('with text', () => (
-    <Button onClick={action('clicked')}>Text Button</Button>
+    <div className={select('theme', themes, styles.dark)}>
+      <Button onClick={action('clicked')}>Text Button</Button>
+    </div>
   ))
   .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        👍
-      </span>
-    </Button>
+    <div className={select('theme', themes, styles.dark)}>
+      <Button onClick={action('clicked')}>
+        <span role="img" aria-label="so cool">
+          👍
+        </span>
+      </Button>
+    </div>
   ))
-  .add('Close Button', () => <CloseButton onClick={action('clicked')} />)
-  .add('Info Button', () => <InfoButton onClick={action('clicked')} />);
+  .add('Close Button', () => (
+    <div className={select('theme', themes, styles.dark)}>
+      <CloseButton onClick={action('clicked')} />
+    </div>
+  ))
+  .add('Info Button', () => (
+    <div className={select('theme', themes, styles.dark)}>
+      <InfoButton onClick={action('clicked')} />
+    </div>
+  ));
 
-storiesOf('Load Mask', module).add('Load Mask', () => <LoadMask />);
+storiesOf('Load Mask', module)
+  // .addDecorator(withInfo)
+  .add('Load Mask', () => (
+    <div className={select('theme', themes, styles.dark)}>
+      <LoadMask />
+    </div>
+  ));
