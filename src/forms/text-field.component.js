1f529c5ee9066069874cf0815d636a889bf5b7a8
Mark Small <mark.small@astrosat.space>
2019-11-06 00:01:34 +0000

    WIP feat(frontend): Some Title text
    Describe why you did what you did
    
    IssueID #

--- src/forms/text-field.component.js
+++ src/forms/text-field.component.js
@@ -6,11 +6,15 @@ import InvalidIcon from './invalid.svg';
 import styles from './text-field.module.css';
 
 const Textfield = ({
+  name,
+  value,
   onChange,
   disabled = false,
   placeholder = '',
   valid,
-  classNames
+  classNames,
+  required = false,
+  autoFocus = false
 }) => {
   let classes = [styles.textfield];
   if (classNames) classes = [...classes, ...classNames];
@@ -18,10 +22,15 @@ const Textfield = ({
   return (
     <div>
       <input
+        type="text"
+        name={name}
+        value={value}
         className={classes}
         disabled={disabled}
         onChange={onChange}
         placeholder={placeholder}
+        required={required}
+        autoFocus={autoFocus}
       />
       {valid && <ValidIcon className={`${styles.icon} ${styles.valid}`} />}
       {valid !== undefined && !valid && (
