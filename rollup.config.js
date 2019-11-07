93234208710a7b384240fb3f514ec6613b651912
Mark Small <mark.small@astrosat.space>
2019-11-07 22:28:15 +0000

    feat(frontend): Remove flow support
    I've removed flow support as I'm not using it.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@
 import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
 import { eslint } from 'rollup-plugin-eslint';
 import svg from 'rollup-plugin-svg';
 
@@ -10,13 +12,41 @@ import pkg from './package.json';
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.browser, format: 'umd', name: 'astrosatui' },
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
+    {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'astrosatui',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'prop-types': 'PropTypes',
+        zxcvbn: 'zxcvbn'
+      }
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'prop-types': 'PropTypes',
+        zxcvbn: 'zxcvbn'
+      }
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'prop-types': 'PropTypes',
+        zxcvbn: 'zxcvbn'
+      }
+    }
   ],
   external: ['react', 'react-dom', 'prop-types', 'zxcvbn'],
   plugins: [
-    resolve(),
+    external(),
     svg(),
     eslint({
       exclude: ['src/**/*.css', 'src/**/*.svg']
@@ -25,7 +55,13 @@ export default {
       modules: true
     }),
     babel({
+      presets: [
+        ['@babel/preset-env', { modules: false }],
+        '@babel/preset-react'
+      ],
       exclude: 'node_modules/**'
-    })
+    }),
+    resolve(),
+    commonjs()
   ]
 };
