86c813bc5cf30460cd3e796d388e794df65759c4
Mark Small <mark.small@astrosat.space>
2019-11-20 20:02:06 +0000

    fix(frontend): Add additional props to TextArea
    I forgot to add some normal attributes to the text area component,
    I've now added them.

--- src/forms/text-area.component.js
+++ src/forms/text-area.component.js
@@ -3,23 +3,36 @@ import React from 'react';
 import styles from './text-area.module.css';
 
 const TextArea = ({
+  name,
+  value,
   onChange,
   disabled = false,
-  rows,
   placeholder = '',
-  classNames
+  classNames,
+  rows,
+  cols,
+  required = false,
+  autoFocus = false,
+  readOnly = false
 }) => {
   let classes = [styles.textarea];
   if (classNames) classes = [...classes, ...classNames];
 
   return (
     <textarea
+      name={name}
       className={classes}
       disabled={disabled}
       onChange={onChange}
       rows={rows}
+      cols={cols}
       placeholder={placeholder}
-    />
+      required={required}
+      autoFocus={autoFocus}
+      readOnly={readOnly}
+    >
+      {value}
+    </textarea>
   );
 };
 
