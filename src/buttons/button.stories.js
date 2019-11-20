867e2c56bc9cbf4f15ea460259e1c4339b6c685a
Mark Small <mark.small@astrosat.space>
2019-11-20 13:18:38 +0000

    fix(frontend): Refactor Button stores to be easier
    I feel I split the various buttons too much, it is quite nice to see
    a number of types of buttons, styles and button content altogether.

--- src/buttons/button.stories.js
+++ src/buttons/button.stories.js
@@ -4,6 +4,15 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, select } from '@storybook/addon-knobs';
 
+import ContactIcon from '../icons/contact-icon.component';
+import CorrectIcon from '../icons/correct-icon.component';
+import ErrorIcon from '../icons/error-icon.component';
+import FacebookIcon from '../icons/facebook-icon.component';
+import LinkedInIcon from '../icons/linked-in-icon.component';
+import MenuIcon from '../icons/menu-icon.component';
+import TwitterIcon from '../icons/twitter-icon.component';
+import CloseIcon from '../icons/close-icon.component';
+
 import Button from './button.component';
 import CloseButton from './close-button.component';
 import InfoButton from './info-button.component';
@@ -14,44 +23,125 @@ const themes = { Dark: styles.dark, Light: styles.light };
 
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
-  .add('with text', () => (
+  .add('Buttons', () => (
     <div className={select('theme', themes, styles.dark)}>
-      <div className={styles.btnGroup}>
-        <Button theme="primary" onClick={action('clicked')}>
-          Primary
-        </Button>
-        <Button theme="secondary" onClick={action('clicked')}>
-          Secondary
-        </Button>
-        <Button theme="tertiary" onClick={action('clicked')}>
-          Tertiary
-        </Button>
-        <Button theme="primary" onClick={action('clicked')} disabled={true}>
-          Disabled
-        </Button>
-        <Button href={action('clicked')}>Link</Button>
-        <Button theme="link" href={action('clicked')}>
-          Link
-        </Button>
-      </div>
-    </div>
-  ))
-  .add('with emoji', () => (
-    <div className={select('theme', themes, styles.dark)}>
-      <Button onClick={action('clicked')}>
-        <span role="img" aria-label="so cool">
-          😀 😎 👍 💯
-        </span>
-      </Button>
-    </div>
-  ))
-  .add('Close Button', () => (
-    <div className={select('theme', themes, styles.dark)}>
-      <CloseButton onClick={action('clicked')} />
-    </div>
-  ))
-  .add('Info Button', () => (
-    <div className={select('theme', themes, styles.dark)}>
-      <InfoButton onClick={action('clicked')} />
+      <fieldset>
+        <legend>Text Buttons</legend>
+
+        <div className={styles.btnGroup}>
+          <Button theme="primary" onClick={action('clicked')}>
+            Primary
+          </Button>
+          <Button theme="secondary" onClick={action('clicked')}>
+            Secondary
+          </Button>
+          <Button theme="tertiary" onClick={action('clicked')}>
+            Tertiary
+          </Button>
+          <Button theme="primary" onClick={action('clicked')} disabled={true}>
+            Disabled
+          </Button>
+          <Button href={action('clicked')}>Link</Button>
+          <Button theme="link" href={action('clicked')}>
+            Link
+          </Button>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Emoji Buttons</legend>
+        <div className={styles.btnGroup}>
+          <Button onClick={action('clicked')}>
+            <span role="img" aria-label="so cool">
+              😀
+            </span>
+          </Button>
+          <Button onClick={action('clicked')}>
+            <span role="img" aria-label="so cool">
+              😎
+            </span>
+          </Button>
+          <Button onClick={action('clicked')}>
+            <span role="img" aria-label="so cool">
+              👍
+            </span>
+          </Button>
+          <Button onClick={action('clicked')}>
+            <span role="img" aria-label="so cool">
+              💯
+            </span>
+          </Button>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Monochrome Icon Buttons</legend>
+        <div className={styles.btnGroup}>
+          <Button onClick={action('clicked')}>
+            <ContactIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <MenuIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <ErrorIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <CorrectIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <CloseIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <TwitterIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <FacebookIcon classes={styles.btnIcon} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <LinkedInIcon classes={styles.btnIcon} />
+          </Button>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Styled Icon Buttons</legend>
+        <div className={styles.btnGroup}>
+          <Button onClick={action('clicked')}>
+            <ContactIcon classes={`${styles.btnIcon} ${styles.red}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <MenuIcon classes={`${styles.btnIcon} ${styles.grey}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <ErrorIcon classes={`${styles.btnIcon} ${styles.blue}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <CorrectIcon classes={`${styles.btnIcon} ${styles.green}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <CloseIcon classes={`${styles.btnIcon} ${styles.teal}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <TwitterIcon classes={`${styles.btnIcon} ${styles.yellow}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <FacebookIcon classes={`${styles.btnIcon} ${styles.orange}`} />
+          </Button>
+          <Button onClick={action('clicked')}>
+            <LinkedInIcon classes={`${styles.btnIcon} ${styles.violet}`} />
+          </Button>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Miscellaneous Buttons</legend>
+        <div className={styles.btnGroup}>
+          <CloseButton onClick={action('clicked')} />
+        </div>
+        <div className={styles.btnGroup}>
+          <InfoButton onClick={action('clicked')} />
+        </div>
+      </fieldset>
     </div>
   ));
