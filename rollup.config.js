a96f956eee77acf6f82847fe304127be83091f1e
Jakub Hampl <kopomir@gmail.com>
2019-11-27 15:43:56 +0000

    chore(frontend): Changes bundling strategy for smaller bundles
    Removes UMD builds. Adds individual modules for smaller components.

--- rollup.config.js
+++ rollup.config.js
@@ -6,50 +6,53 @@ import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import { eslint } from 'rollup-plugin-eslint';
 import svg from 'rollup-plugin-svg';
-
+import glob from 'glob';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'astrosatui',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'prop-types': 'PropTypes',
-        zxcvbn: 'zxcvbn'
+const plugins = [
+  external({ includeDependencies: true }),
+  svg(),
+  eslint({
+    exclude: ['src/**/*.css', 'src/**/*.svg']
+  }),
+  postcss({
+    modules: true
+  }),
+  babel({
+    presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+    exclude: 'node_modules/**'
+  }),
+  resolve(),
+  commonjs()
+];
+
+export default [
+  {
+    input: Object.fromEntries(
+      glob
+        .sync('src/*/*.component.js')
+        .map(p => [p.match(/\/(.+?)\.component\.js$/)[1], p])
+    ),
+    output: [
+      {
+        dir: 'dist',
+        format: 'es'
       }
-    },
-    {
-      file: pkg.main,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  external: ['react', 'react-dom', 'prop-types', 'zxcvbn'],
-  plugins: [
-    external({ includeDependencies: true }),
-    svg(),
-    eslint({
-      exclude: ['src/**/*.css', 'src/**/*.svg']
-    }),
-    postcss({
-      modules: true
-    }),
-    babel({
-      presets: [
-        ['@babel/preset-env', { modules: false }],
-        '@babel/preset-react'
-      ],
-      exclude: 'node_modules/**'
-    }),
-    resolve(),
-    commonjs()
-  ]
-};
+    ],
+    plugins
+  },
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    plugins
+  }
+];
