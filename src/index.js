017e21ee05a23f5da9b2f9f3ef90ebf277c4019d
Mark Small <mark.small@astrosat.space>
2019-12-12 13:27:14 +0000

    fix(frontend): Export components forgotten about
    The library has a number of components that where not being exported
    here. I've now updated the index.js file to export every component
    and hook.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 export { default as LoadMask } from './load-mask/load-mask.component';
 
 export { default as Button } from './buttons/button.component';
-export { default as ButtonToggle } from './buttons/button-toggle.component';
+export { default as ButtonGroup } from './buttons/button-group.component';
 export { default as CloseButton } from './buttons/close-button.component';
 export { default as InfoButton } from './buttons/info-button.component';
 
 export { default as Checkbox } from './forms/checkbox.component';
 export { default as PasswordField } from './forms/password-field.component';
+export { default as PasswordStrengthMeter } from './forms/password-strength-meter.component';
 export { default as Radio } from './forms/radio.component';
+export { default as Slider } from './forms/slider.component';
 export { default as TextArea } from './forms/text-area.component';
 export { default as Textfield } from './forms/text-field.component';
 
