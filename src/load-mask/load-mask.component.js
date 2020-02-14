a812cd8ba90e1fc508e8a5b4354c584c18d9cf1f
Rory MacGregor <rorymacgregor@astrosat.net>
2020-02-14 12:37:44 +0000

    fix(frontend): Change loadmask color/edit slider
    I removed the yellow from the loadmask and replaced it with white, so it is now dark with white.
    I also removed the padding from the slider and increased its height.

--- src/load-mask/load-mask.component.js
+++ src/load-mask/load-mask.component.js
@@ -5,13 +5,7 @@ import styles from './load-mask.module.css';
 const LoadMask = () => (
   <div className={styles['load-mask']}>
     <svg width="100px" height="100px" viewBox="0 0 1000 1000">
-      <circle
-        cx="500"
-        cy="570"
-        r="80"
-        stroke="#fff"
-        fill="rgba(246, 190, 0, 0.8)"
-      />
+      <circle cx="500" cy="570" r="80" stroke="#fff" fill="#fff" />
       <circle
         cx="500"
         cy="570"
@@ -37,31 +31,13 @@ const LoadMask = () => (
         fill="none"
       />
       <g className={styles.spinner3}>
-        <circle
-          cx="300"
-          cy="570"
-          r="30"
-          stroke="#fff"
-          fill="rgba(246, 190, 0, 0.8)"
-        />
+        <circle cx="300" cy="570" r="30" stroke="#fff" fill="#fff" />
       </g>
       <g className={styles.spinner4}>
-        <circle
-          cx="500"
-          cy="366"
-          r="30"
-          stroke="#fff"
-          fill="rgba(246, 190, 0, 0.8)"
-        />
+        <circle cx="500" cy="366" r="30" stroke="#fff" fill="#fff" />
       </g>
       <g className={styles.spinner5}>
-        <circle
-          cx="500"
-          cy="30"
-          r="30"
-          stroke="#fff"
-          fill="rgba(246, 190, 0, 0.8)"
-        />
+        <circle cx="500" cy="30" r="30" stroke="#fff" fill="#fff" />
       </g>
     </svg>
   </div>
