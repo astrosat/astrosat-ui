0cac1e76f7108ea2a0e2d40c158bd09652720355
Reignable <joebarrett401@gmail.com>
2020-04-17 10:41:25 +0100

    fix(ThemeProvider): Change theme displaying method
    Changed ThemeProvider to set the theme on the body
    This lets anything created with portals use the theme correctly

--- src/containers/theme-provider.component.js
+++ src/containers/theme-provider.component.js
@@ -1,14 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styles from '../index.module.css';
 
 const ThemeContext = React.createContext();
 
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('dark');
+const ThemeProvider = ({ children, defaultTheme = 'dark' }) => {
+  const [theme, setTheme] = useState(defaultTheme);
+
+  useEffect(() => {
+    changeTheme(theme);
+  }, []);
+
+  const changeTheme = newTheme => {
+    if (!styles[newTheme]) {
+      console.warn(
+        `You've tried to set the theme to ${newTheme} which doesn't exist. Please check the theme name and try again`
+      );
+    }
+    if (!document.body.classList.contains(styles[newTheme])) {
+      if (document.body.classList.contains(styles[theme])) {
+        document.body.classList.replace(styles[theme], styles[newTheme]);
+      } else {
+        document.body.classList.add(styles[newTheme]);
+      }
+      setTheme(newTheme);
+    }
+  };
 
   return (
-    <ThemeContext.Provider value={[theme, setTheme]}>
-      <div className={styles[theme]}>{children}</div>
+    <ThemeContext.Provider value={[theme, changeTheme]}>
+      {children}
     </ThemeContext.Provider>
   );
 };
