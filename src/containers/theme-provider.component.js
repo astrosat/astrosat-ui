d755303aaaa3a00571e6d8ebdfc98c92842ed986
Reignable <joebarrett401@gmail.com>
2020-04-20 08:35:01 +0100

    fix(ThemeProvider): Fix hook dependencies
    Added the correct dependencies to ThemeProvider's useEffect
    Wrap changeTheme in useCallback for memoisation
    
    IssueID #27

--- src/containers/theme-provider.component.js
+++ src/containers/theme-provider.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useCallback, useContext, useState, useEffect } from 'react';
 import styles from '../index.module.css';
 
 const ThemeContext = React.createContext();
@@ -6,25 +6,28 @@ const ThemeContext = React.createContext();
 const ThemeProvider = ({ children, defaultTheme = 'dark' }) => {
   const [theme, setTheme] = useState(defaultTheme);
 
+  const changeTheme = useCallback(
+    newTheme => {
+      if (!styles[newTheme]) {
+        console.warn(
+          `You've tried to set the theme to ${newTheme} which doesn't exist. Please check the theme name and try again`
+        );
+      }
+      if (!document.body.classList.contains(styles[newTheme])) {
+        if (document.body.classList.contains(styles[theme])) {
+          document.body.classList.replace(styles[theme], styles[newTheme]);
+        } else {
+          document.body.classList.add(styles[newTheme]);
+        }
+        setTheme(newTheme);
+      }
+    },
+    [theme, setTheme]
+  );
+
   useEffect(() => {
     changeTheme(theme);
-  }, []);
-
-  const changeTheme = newTheme => {
-    if (!styles[newTheme]) {
-      console.warn(
-        `You've tried to set the theme to ${newTheme} which doesn't exist. Please check the theme name and try again`
-      );
-    }
-    if (!document.body.classList.contains(styles[newTheme])) {
-      if (document.body.classList.contains(styles[theme])) {
-        document.body.classList.replace(styles[theme], styles[newTheme]);
-      } else {
-        document.body.classList.add(styles[newTheme]);
-      }
-      setTheme(newTheme);
-    }
-  };
+  }, [theme, changeTheme]);
 
   return (
     <ThemeContext.Provider value={[theme, changeTheme]}>
