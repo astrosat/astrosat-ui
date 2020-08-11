18567d10bc13665ae02738dd64d7faed4e1f5c4d
Reignable <joebarrett401@gmail.com>
2020-08-11 15:00:53 +0100

    fix(TextArea): Add rest prop

--- src/forms/text-area.component.js
+++ src/forms/text-area.component.js
@@ -2,42 +2,12 @@ import React from 'react';
 
 import styles from './text-area.module.css';
 
-const TextArea = (
-  {
-    name,
-    value,
-    onChange,
-    disabled = false,
-    placeholder = '',
-    classNames,
-    rows,
-    cols,
-    required = false,
-    autoFocus = false,
-    readOnly = false,
-    ariaLabel
-  },
-  ref
-) => {
+const TextArea = ({ classNames, ariaLabel, ...rest }, ref) => {
   let classes = [styles.textarea];
   if (classNames) classes = [...classes, ...classNames];
 
   return (
-    <textarea
-      ref={ref}
-      name={name}
-      value={value}
-      className={classes}
-      disabled={disabled}
-      onChange={onChange}
-      rows={rows}
-      cols={cols}
-      placeholder={placeholder}
-      aria-label={ariaLabel}
-      required={required}
-      autoFocus={autoFocus}
-      readOnly={readOnly}
-    ></textarea>
+    <textarea ref={ref} className={classes} aria-label={ariaLabel} {...rest} />
   );
 };
 
