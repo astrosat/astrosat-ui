85a6f8addbe7c71e759fb7be455df3f290c86d4c
Reignable <joebarrett401@gmail.com>
2020-09-21 11:20:08 +0100

    fix(frontend): Add more theme values
    IssueID #64

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -1,15 +1,27 @@
 import { createMuiTheme } from '@material-ui/core';
+import createPalette from '@material-ui/core/styles/createPalette';
+
+const palette = createPalette({
+  type: 'dark',
+  primary: { main: '#f6be00' },
+  secondary: { main: '#333f48' },
+  success: { main: '#6cc24a' },
+  error: { main: '#cf6679' },
+  info: { main: '#48a9c5' },
+  text: { primary: '#333f48', secondary: '#f8f8f8' }
+});
 
 export const dark = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: { main: '#f6be00' },
-    secondary: { main: '#333f48' },
-    success: { main: '#6cc24a' },
-    error: { main: '#cf6679' },
-    info: { main: '#48a9c5' }
+  palette,
+  shape: {
+    borderRadius: 5
   },
   typography: {
-    fontFamily: "'Open Sans' sans-serif"
+    fontFamily: '"Open Sans", sans-serif',
+    button: {
+      fontSize: '18px',
+      fontWeight: 600,
+      textTransform: 'none'
+    }
   }
 });
