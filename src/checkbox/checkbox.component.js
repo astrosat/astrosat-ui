fa86a9292a65271419a3372ec8852758c962e1af
Rory MacGregor <rorymacgregor@astrosat.net>
2020-09-23 17:54:39 +0100

    fix(frontend): Flesh out component, tests and story
    Fleshed out the various files, and updated the component. Most notable, adding additional props (aria-label, for example), and switching out th edefault SVG icon for a custom styled icon.

--- src/checkbox/checkbox.component.js
+++ src/checkbox/checkbox.component.js
@@ -1,40 +1,72 @@
 import React from 'react';
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { Checkbox as MuiCheckbox } from '@material-ui/core';
+import { Checkbox as MuiCheckbox, CheckboxProps } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const styles = makeStyles(theme => ({
+const labelStyles = makeStyles(theme => ({
   root: {
-    '& .MuiSvgIcon-root': {
-      fill: `${theme.palette.background.default} !important`
+    color: theme.palette.secondary.main,
+    '& .Mui-disabled': {
+      color: theme.palette.secondary.main,
+      opacity: 0.4,
+      cursor: 'not-allowed'
     }
   },
-  checked: {
-    color: `${theme.palette.primary.main} !important`
+  label: {
+    fontSize: '1rem'
   },
-  disabled: {
-    color: '#008000 !important'
-  },
-  transition: theme.transitions.create(
-    ['background-color', 'box-shadow', 'border', 'opacity'],
-    {
-      duration: theme.transitions.duration.short
-    }
-  )
+  disabled: {}
 }));
 
-const Checkbox = ({ name, label = '', disabled = false }, ref) => {
-  const classes = styles({});
+const checkboxStyles = makeStyles(theme => ({
+  root: {},
+  icon: {
+    width: '1rem',
+    height: '1rem',
+    borderRadius: '3px',
+    border: `1px solid ${theme.palette.secondary.main}`
+  },
+  checkedIcon: {
+    width: '1rem',
+    height: '1rem',
+    borderRadius: '3px',
+    border: `1px solid ${theme.palette.primary.main}`,
+    '&::after': {
+      display: 'block',
+      position: 'absolute',
+      left: '0.125rem',
+      bordeRadius: '0.1875rem',
+      content: '""',
+      width: '0.75rem',
+      height: '0.75rem',
+      transition: 'background-color 250ms ease, opacity 250ms ease'
+    },
+    '&$checked::after': {
+      backgroundColor: 'red'
+    }
+  },
+  checked: {},
+  disabled: {}
+}));
+
+const Checkbox = ({ label = '', disabled = false, ...rest }, ref) => {
+  const checkboxClasses = checkboxStyles({});
+  const labelClasses = labelStyles({});
+  console.log('CheckboxClasses: ', checkboxClasses);
   return (
     <FormControlLabel
+      classes={labelClasses}
       control={
         <MuiCheckbox
-          name={name}
-          classes={classes}
+          classes={checkboxClasses}
+          checkedIcon={<span className={checkboxClasses.checkedIcon} />}
+          icon={<span className={checkboxClasses.icon} />}
           disabled={disabled}
-          ref={ref}
+          inputRef={ref}
+          inputProps={{ 'aria-label': `${label} checkbox` }}
+          {...rest}
         />
       }
       label={label}
--- src/checkbox/checkbox.test.js
+++ src/checkbox/checkbox.test.js
@@ -5,12 +5,62 @@ import { cleanup, render, fireEvent } from '@testing-library/react';
 import Checkbox from './checkbox.component';
 
 describe('Checkbox Component', () => {
+  let onChange = jest.fn();
+
   afterEach(cleanup);
 
-  it('should render a checkbox tag with text under normal circumstances', () => {
-    const { container, getByText } = render(<Checkbox />);
+  it('should render a checkbox tag with label text', () => {
+    const { container, getByText } = render(
+      <Checkbox name="Test Name" label="Test Label" onChange={onChange} />
+    );
 
-    expect(container.querySelector('checkbox')).toBeInTheDocument();
-    expect(getByText('Some Text')).toBeInTheDocument();
+    expect(container.querySelector('input')).toBeInTheDocument();
+    expect(getByText('Test Label')).toBeInTheDocument();
+  });
+
+  it('should render a checked checkbox if `checked` prop is present', () => {
+    const { container } = render(
+      <Checkbox
+        name="Test Name"
+        label="Test Label"
+        onChange={onChange}
+        checked
+      />
+    );
+
+    expect(container.querySelector('input')).toHaveAttribute('checked');
+  });
+
+  it('should call `onChange` function when checked/unchecked', () => {
+    const { container } = render(
+      <Checkbox name="Test Name" label="Test Label" onChange={onChange} />
+    );
+
+    fireEvent.click(container.querySelector('input'));
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('should also call `onChange` function when label is clicked', () => {
+    const { getByText } = render(
+      <Checkbox name="Test Name" label="Test Label" onChange={onChange} />
+    );
+
+    fireEvent.click(getByText('Test Label'));
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('should disable checkbox when `disabled` prop is present', () => {
+    const { container } = render(
+      <Checkbox
+        name="Test Name"
+        label="Test Label"
+        onChange={onChange}
+        disabled
+      />
+    );
+
+    expect(container.querySelector('input')).toHaveAttribute('disabled');
   });
 });
