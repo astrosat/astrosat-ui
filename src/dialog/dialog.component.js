baeb2d2c9834da6301ca9108d0a1d828a4d32aa0
Rory MacGregor <rorymacgregor@astrosat.net>
2020-09-29 13:35:09 +0100

    fix(frontend): Finish dialog component
    Finished building the dialog component. A few areas probably needing discussion,
    but it's built, works as intended and has stories demonstrating various situations.

--- src/dialog/dialog.component.js
+++ src/dialog/dialog.component.js
@@ -1,11 +1,78 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 
-import { Dialog as MuiDialog } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Dialog = ({ children, open = false, ...props }, ref) => {
+import {
+  Dialog as MuiDialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+} from '@material-ui/core';
+
+const titleStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    height: '7rem',
+    width: '100%',
+    color: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main
+  }
+}));
+
+const contentStyles = makeStyles(() => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    padding: '1.25rem'
+  }
+}));
+
+const actionsStyles = makeStyles(() => ({
+  root: {
+    padding: '1.25rem'
+  }
+}));
+
+const dialogStyles = makeStyles(theme => ({
+  paper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    minWidth: '46.5rem',
+    maxHeight: '46.5rem',
+    color: theme.palette.text.primary,
+    borderRadius: theme.shape.borderRadius
+  }
+}));
+
+const Dialog = (
+  { children, title = '', open = false, actions, ...props },
+  ref
+) => {
+  const titleClasses = titleStyles({});
+  const contentClasses = contentStyles({});
+  const actionsClasses = actionsStyles({});
+  const dialogClasses = dialogStyles({});
   return (
-    <MuiDialog open={open} {...props} ref={ref}>
-      {children}
+    <MuiDialog
+      open={open}
+      {...props}
+      classes={dialogClasses}
+      transitionDuration={{ enter: 0, exit: 0 }}
+      ref={ref}
+    >
+      <DialogTitle classes={titleClasses} disableTypography>
+        {title}
+      </DialogTitle>
+      <DialogContent classes={contentClasses}>{children}</DialogContent>
+      {actions && (
+        <DialogActions classes={actionsClasses}>{actions}</DialogActions>
+      )}
     </MuiDialog>
   );
 };
--- src/dialog/dialog.stories.js
+++ src/dialog/dialog.stories.js
@@ -4,17 +4,117 @@ import { default as Button } from '../button/button.component';
 
 import { default as Dialog } from './dialog.component';
 
+import styles from './dialog.module.css';
+
 export default { title: 'Dialog' };
 
 export const Default = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [dialog, setDialog] = useState(null);
   const ref = useRef();
+
+  const closeButton = <Button onClick={() => setDialog(null)}>Close</Button>;
+
+  const tallContent = (
+    <div className={styles.container}>
+      {[...Array(20)].map(() => (
+        <h2 className={styles.text}>This is tall content</h2>
+      ))}
+    </div>
+  );
+
+  const wideContent = (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        This is wide content This is wide content This is wide content This is
+        wide content This is wide content This is wide content This is wide
+        content This is wide content This is wide content This is wide content
+        This is wide content This is wide content
+      </p>
+    </div>
+  );
+
+  const tallAndWideContent = (
+    <>
+      <div className={styles.boxContainer}>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+      </div>
+      <div className={styles.boxContainer}>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+      </div>
+      <div className={styles.boxContainer}>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+        <div className={styles.box}>Box</div>
+      </div>
+    </>
+  );
+
+  const noActionContent = (
+    <div className={styles.container}>
+      <h1 className={styles.text}>This dialog has no action buttons</h1>
+    </div>
+  );
+
   return (
     <>
-      <Button onClick={() => setIsVisible(true)}>Open Dialog</Button>
+      <div className={styles.buttons}>
+        <Button
+          onClick={() =>
+            setDialog({
+              CONTENT: tallContent,
+              ACTIONS: closeButton
+            })
+          }
+        >
+          Tall Content
+        </Button>
+        <Button
+          onClick={() =>
+            setDialog({
+              CONTENT: wideContent,
+              ACTIONS: closeButton
+            })
+          }
+        >
+          Wide Content
+        </Button>
+        <Button
+          onClick={() =>
+            setDialog({
+              CONTENT: tallAndWideContent,
+              ACTIONS: closeButton
+            })
+          }
+        >
+          Tall and Wide Content
+        </Button>
+        <Button
+          onClick={() =>
+            setDialog({
+              CONTENT: noActionContent,
+              ACTIONS: null
+            })
+          }
+        >
+          No Actions
+        </Button>
+      </div>
 
-      <Dialog open={isVisible} ref={ref}>
-        <div>I Am A Dialog</div>
+      <Dialog
+        open={dialog}
+        onBackdropClick={() => setDialog(null)}
+        title="Dialog Title"
+        ref={ref}
+        actions={dialog?.ACTIONS}
+      >
+        {dialog?.CONTENT}
       </Dialog>
     </>
   );
