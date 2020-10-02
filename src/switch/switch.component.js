457a65729441c6fab6d76d7d63b9964de006f5df
Slawomir Dyk <slawomir.dyk@astrosat.net>
2020-10-02 08:18:44 +0100

    Added different colours for each switch, styled a small size switch

--- src/switch/switch.component.js
+++ src/switch/switch.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Switch as MuiSwitch, makeStyles } from '@material-ui/core';
 
 const useStyle = makeStyles(theme => ({
@@ -17,13 +18,42 @@ const useStyle = makeStyles(theme => ({
     height: 16,
     width: 30,
     padding: 0,
-    display: 'flex'
+    display: 'flex',
+    '&$smallSize': {
+      backgroundColor: 'yellow'
+    }
   },
   switchBase: {
     padding: 1,
     '&$checked': {
       transform: 'translateX(14px)',
       '& + $track': { backgroundColor: theme.palette.success.main, opacity: 1 }
+    }
+  },
+  colorSecondary: {
+    '&$checked + $track': {
+      backgroundColor: theme.palette.error.main
+    }
+  },
+  colorPrimary: {
+    '&$checked + $track': {
+      backgroundColor: theme.palette.primary.main
+    }
+  },
+  sizeSmall: {
+    width: 26,
+    height: 11,
+    padding: 0,
+    '& $thumb': {
+      width: 10,
+      height: 10,
+      boxShadow: 'none'
+    },
+    '& $switchBase': {
+      padding: 1.5,
+      '&$checked': {
+        transform: 'translateX(11px)'
+      }
     }
   }
 }));
--- src/switch/switch.stories.js
+++ src/switch/switch.stories.js
@@ -1,25 +1,48 @@
 import React from 'react';
 
 import Switch from './switch.component';
-import { default as OldSwitch } from '../buttons/switch.component';
+
+import { FormControlLabel } from '@material-ui/core';
+
+import FormGroup from '@material-ui/core/FormGroup';
 
 export default { title: 'Switch' };
 
 export const Color = () => (
-  <>
-    <OldSwitch />
-    <Switch />
-    <Switch color="default" />
-    <Switch color="primary" />
-    <Switch color="secondary" />
-  </>
+  <FormGroup>
+    <FormControlLabel
+      label="Default Color"
+      control={<Switch color="default" />}
+    ></FormControlLabel>
+    <FormControlLabel
+      label="Primary Color"
+      control={<Switch color="primary" />}
+    ></FormControlLabel>
+    <FormControlLabel
+      label="Secondary Color"
+      control={<Switch color="secondary" />}
+    ></FormControlLabel>
+  </FormGroup>
 );
 
-export const Disabled = () => <Switch disabled />;
+export const Disabled = () => (
+  <FormGroup>
+    <FormControlLabel
+      label="Disabled"
+      control={<Switch disabled />}
+    ></FormControlLabel>
+  </FormGroup>
+);
 
 export const Size = () => (
-  <>
-    <Switch size="medium" />
-    <Switch size="small" />
-  </>
+  <FormGroup>
+    <FormControlLabel
+      label="Medium Size"
+      control={<Switch size="medium" color="default" />}
+    ></FormControlLabel>
+    <FormControlLabel
+      label="Small Size"
+      control={<Switch size="small" color="default" />}
+    ></FormControlLabel>
+  </FormGroup>
 );
