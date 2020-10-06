cb7e2b34cfab79de408743df311622ca0e4de652
Slawomir Dyk <slawomir.dyk@astrosat.net>
2020-10-06 13:33:27 +0100

    Add stories example

--- src/well/well.component.js
+++ src/well/well.component.js
@@ -1,1 +1,16 @@
-import React from 'react';
+// import React from 'react';
+
+// import { Snackbar } from '@material-ui/core';
+
+// import Alert from '@material-ui/core'
+
+// const Well = (props, ref) => {
+//   // const classes = useStyles({});
+
+//   return <Snackbar ref={ref} {...props} >
+//     <Alert  >
+//     This is a success message!
+//   </Alert>
+//   </Snackbar>;
+// };
+// export default React.forwardRef(Well);
