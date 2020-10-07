ed1b55db6b9f83549b6ac18ae5ecb5a7bc4124fb
Reignable <joebarrett401@gmail.com>
2020-10-07 13:51:11 +0100

    feat(frontend): Configure typescript and build process
    Configured typescript to produce d.ts files at build time
    Cleaned up the build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,17 @@
 // Rollup plugins
-import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import includePaths from 'rollup-plugin-includepaths';
 import { eslint } from 'rollup-plugin-eslint';
-import svg from 'rollup-plugin-svg';
-import glob from 'glob';
+import includePaths from 'rollup-plugin-includepaths';
+import resolve from 'rollup-plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 const plugins = [
   includePaths({ paths: ['src'] }),
   external({ includeDependencies: true }),
-  svg(),
   eslint({
     exclude: ['src/**/*.css', 'src/**/*.svg']
   }),
@@ -25,38 +23,21 @@ const plugins = [
     exclude: 'node_modules/**'
   }),
   resolve(),
-  commonjs()
+  commonjs(),
+  typescript()
 ];
 
-// NOTE: I've switched to CommonJS modules as opposed to my preferred
-//       option of ES2015 modules, a newer and better system as when
-export default [
-  {
-    input: Object.fromEntries(
-      glob
-        .sync('src/**/*.{component,hook}.js')
-        .map(p => [p.match(/\/(.+?)\.(component|hook)\.js$/)[1], p])
-    ),
-    output: [
-      {
-        dir: 'dist',
-        format: 'cjs'
-      }
-    ],
-    plugins
-  },
-  {
-    input: 'src/index.js',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs'
-      },
-      {
-        file: pkg.module,
-        format: 'es'
-      }
-    ],
-    plugins
-  }
-];
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins
+};
