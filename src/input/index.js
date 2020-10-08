0ca6477b7b4abe546285f93ed78f0873eeb953cc
Rory MacGregor <rorymacgregor@astrosat.net>
2020-10-08 13:16:23 +0100

    fix(frontend): Remove default import
    Removed default import from `InputAdornment` after switching to Material import.

--- src/input/index.js
+++ src/input/index.js
@@ -1,5 +1,5 @@
 import { default as Input } from './input.component';
-import { default as InputAdornment } from '@material-ui/core';
+import { InputAdornment } from '@material-ui/core';
 import { default as InputLabel } from './input-label.component';
 
 export { Input, InputAdornment, InputLabel };
