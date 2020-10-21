66558c6fb3a894b91b03581002c1abcdca4589b5
Slawomir Dyk <slawomir.dyk@astrosat.net>
2020-10-21 08:42:18 +0100

    Refactored the code in Menu component

--- src/menu/menu.component.js
+++ src/menu/menu.component.js
@@ -1,58 +1,36 @@
-import React, { useState } from 'react';
-
-import Button from '@material-ui/core/Button';
+import React from 'react';
 
 import { Menu as MuiMenu } from '@material-ui/core';
 
-import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/';
 
-import makeStyles from '@material-ui/core';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-
-//   }
-// }));
+const useStyles = makeStyles(theme => ({
+  list: {
+    backgroundColor: theme.palette.background.paper,
+    color: theme.palette.text.secondary,
+    '& li': {
+      padding: '0.3em'
+    },
+    '& li:nth-child(2)': {
+      borderBottom: `1px solid ${theme.palette.text.secondary}`,
+      borderTop: `1px solid ${theme.palette.text.secondary}`
+    }
+  },
+  paper: {
+    width: '100%'
+  }
+}));
 
 /**
  * @param {import('@material-ui/core').MenuProps} props
  * @param {React.Ref<any>} ref
  */
 
-const Menu = ({ ...rest }, ref) => {
-  // const classes = useStyles
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+const Menu = (props, ref) => {
+  const classes = useStyles({});
   return (
     <div>
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Menu
-      </Button>
-      <MuiMenu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        {...rest}
-        ref={ref}
-      >
-        <MenuItem onClick={handleClose}>Option1</MenuItem>
-        <MenuItem onClick={handleClose}>Option2</MenuItem>
-        <MenuItem onClick={handleClose}>Option3</MenuItem>
-      </MuiMenu>
+      <MuiMenu classes={classes} {...props} ref={ref} />
     </div>
   );
 };
--- src/menu/menu.stories.js
+++ src/menu/menu.stories.js
@@ -1,17 +1,17 @@
+import { MenuItem } from '@material-ui/core';
+
 import React from 'react';
 
 import { default as Menu } from './menu.component';
 
-// import { MenuItem } from '@material-ui/core';
-
 export default { title: 'Menu' };
 
 export const Options = () => {
-  return <Menu open={true}></Menu>;
-};
-export const NoOption = () => {
-  return <Menu open={true}></Menu>;
-};
-export const Disabled = () => {
-  return <Menu open={true}></Menu>;
+  return (
+    <Menu open={true}>
+      <MenuItem>Option1</MenuItem>
+      <MenuItem>Option2</MenuItem>
+      <MenuItem>Option3</MenuItem>
+    </Menu>
+  );
 };
