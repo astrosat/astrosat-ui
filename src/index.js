bf977d2e08e313e3664093cc084fc4bd38126a56
Reignable <joebarrett401@gmail.com>
2020-10-26 08:53:57 +0000

    chore(frontend): Sort index file
    IssueID #101

--- src/index.js
+++ src/index.js
@@ -1,49 +1,35 @@
 export {
   FormControl,
-  InputLabel,
   InputAdornment,
+  InputLabel,
   RadioGroup
 } from '@material-ui/core';
-export { default as LoadMask } from './load-mask/load-mask.component';
-
 export { default as Button } from './button/button.component';
-export { default as Checkbox } from './checkbox/checkbox.component';
-
-export { default as Radio } from './radio/radio.component';
-
-export { default as Link } from './link/link.component';
-export { default as Switch } from './switch/switch.component';
-export { default as Well } from './well/well.component';
-
-export { default as FormControlLabel } from './form-control-label/form-control-label.component';
-
-export { default as PasswordInput } from './password-input/password-input.component';
-
-export { default as Input } from './input/input.component';
-export * from './dialog';
-
 export { default as ButtonGroup } from './buttons/button-group.component';
 export { default as CloseButton } from './buttons/close-button.component';
 export { default as InfoButton } from './buttons/info-button.component';
-
-export { default as PasswordStrengthMeter } from './forms/password-strength-meter.component';
-export { default as Slider } from './forms/slider.component';
-export { default as TextArea } from './forms/text-area.component';
-export { default as Select } from './forms/select.component';
-export { default as Textfield } from './forms/text-field.component';
-export { default as useForm } from './forms/use-form.hook';
-
+export { default as Checkbox } from './checkbox/checkbox.component';
 export { default as Detail } from './containers/detail.component';
-export { default as useModal } from './containers/use-modal.hook';
-
-export { default as Pill } from './pill/pill.component';
-export { default as PillAction } from './pill/pill-action.component';
-
-export * from './icons';
-
-export { default as ProgressBar } from './progress/progress-bar.component';
-
 export {
-  ThemeProvider,
-  ThemeContext
+  ThemeContext,
+  ThemeProvider
 } from './containers/theme-provider.component';
+export { default as useModal } from './containers/use-modal.hook';
+export * from './dialog';
+export { default as FormControlLabel } from './form-control-label/form-control-label.component';
+export { default as PasswordStrengthMeter } from './forms/password-strength-meter.component';
+export { default as Select } from './forms/select.component';
+export { default as Slider } from './forms/slider.component';
+export { default as useForm } from './forms/use-form.hook';
+export * from './icons';
+export { default as Input } from './input/input.component';
+export { default as Link } from './link/link.component';
+export { default as LoadMask } from './load-mask/load-mask.component';
+export { default as PasswordInput } from './password-input/password-input.component';
+export { default as PillAction } from './pill/pill-action.component';
+export { default as Pill } from './pill/pill.component';
+export { default as ProgressBar } from './progress/progress-bar.component';
+export { default as Radio } from './radio/radio.component';
+export { default as Switch } from './switch/switch.component';
+export { default as TextField } from './text-field/text-field.component';
+export { default as Well } from './well/well.component';
