0a740cc71c784f116f34f2569be9512a5a57797d
Reignable <joebarrett401@gmail.com>
2020-10-26 16:35:45 +0000

    fix(frontend): Remove old buttons, add to index
    IssueID #72

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as LoadMask } from './load-mask/load-mask.component';
 
 export { default as Button } from './button/button.component';
+export { default as IconButton } from './icon-button/icon-button.component';
 export { default as Checkbox } from './checkbox/checkbox.component';
 
 export { default as Radio } from './radio/radio.component';
@@ -17,10 +18,6 @@ export { default as PasswordInput } from './password-input/password-input.compon
 
 export * from './input';
 export * from './dialog';
-
-export { default as ButtonGroup } from './buttons/button-group.component';
-export { default as CloseButton } from './buttons/close-button.component';
-export { default as InfoButton } from './buttons/info-button.component';
 
 export { default as PasswordStrengthMeter } from './forms/password-strength-meter.component';
 export { default as Slider } from './forms/slider.component';
