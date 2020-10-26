4768bb23402e95ffdc43a8926f9a7e683cd636a1
Reignable <joebarrett401@gmail.com>
2020-10-26 16:31:34 +0000

    feat(frontend): Create and style icon button
    IssueID #72

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,5 +1,5 @@
-import { createMuiTheme } from '@material-ui/core';
-import { merge } from 'lodash';
+import { createMuiTheme, fade } from '@material-ui/core';
+import deepmerge from 'deepmerge';
 import { core } from './core';
 import { palette } from './palette';
 
@@ -9,7 +9,20 @@ const light = {
     type: 'light',
     text: { primary: palette.secondary.main },
     background: { default: palette.grey[100] }
+  },
+  overrides: {
+    MuiIconButton: {
+      root: {
+        color: palette.secondary.main,
+        '&:hover': {
+          backgroundColor: fade(
+            palette.secondary.main,
+            palette.action.hoverOpacity
+          )
+        }
+      }
+    }
   }
 };
 
-export default createMuiTheme(merge(core, light));
+export default createMuiTheme(deepmerge(core, light));
