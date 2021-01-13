bc318d29cceb4a2f764256d645e4b8aab9aaa82e
Reignable <joebarret401@gmail.com>
2021-01-13 16:14:30 +0000

    fix(frontend): Add icons for MSL

---
+++ src/icons/options-alt-icon.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+/**
+ * @param {import('@material-ui/core').SvgIconProps} props
+ */
+const OptionsAltIcon = props => (
+  <SvgIcon {...props}>
+    <path
+      d="M3.111 15.023a3.111 3.111 0 100-6.223 3.111 3.111 0 000 6.223zM12 15.023A3.111 3.111 0 1012 8.8a3.111 3.111 0 000 6.223zM20.889 15.023a3.111 3.111 0 100-6.223 3.111 3.111 0 000 6.223z"
+      fill="currentColor"
+    />
+  </SvgIcon>
+);
+
+export default OptionsAltIcon;
