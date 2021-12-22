9dfe8cccf1ca1d3e681cd825a33011fe24843a20
Mark Small <mark.small@astrosat.space>
2021-12-22 12:59:24 +0000

    fix(frontend): Upgrade library build tool
    We use rollup to build/package our library, I've now upgrade its
    dependencies and fixed any problems that arose from that, changing
    config etc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 // Rollup plugins
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import { eslint } from 'rollup-plugin-eslint';
 import includePaths from 'rollup-plugin-includepaths';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
@@ -19,6 +19,7 @@ const plugins = [
     modules: true
   }),
   babel({
+    babelHelpers: 'runtime', // NOTE: Maybe 'bundled' is the better option
     presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
     exclude: 'node_modules/**'
   }),
