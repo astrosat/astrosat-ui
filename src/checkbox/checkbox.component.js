144aff6886fb0058ee97f17339e6bbc0c358efc0
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-04-20 17:19:52 +0100

    upgrade the checkbox and radio buttons to the new astrosat-ui version
    I rebuilt the checkbox and radio buttons components according to the new rules for creating them for a new version of material-ui. The $ syntax used with JSS will not work with emotion. I replaced those selectors with valid class selectors.
    
    IssueID: 1137

--- src/checkbox/checkbox.component.js
+++ src/checkbox/checkbox.component.js
@@ -1,84 +1,89 @@
 import React from 'react';
 
-import { Checkbox as MuiCheckbox } from '@mui/material';
+import { Checkbox as MuiCheckbox, Box } from '@mui/material';
 
-import makeStyles from '@mui/styles/makeStyles';
-
-const checkboxStyles = makeStyles(theme => ({
-  root: {
-    padding: 0,
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-    '&$checked:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
-  icon: {
-    width: '1rem',
-    height: '1rem',
-    borderRadius: '0.1875rem',
-    border: `1px solid ${theme.palette.text.primary}`,
-    transition: theme.transitions.create(['border'], {
-      easing: theme.transitions.easing.easeInOut,
-      duration: theme.transitions.duration.short,
-    }),
-    'input:hover ~ &': {
-      border: `1px solid ${theme.palette.primary.main}`,
-    },
-    'input:disabled ~ &': {
-      border: `1px solid ${theme.palette.text.primary}`,
-    },
-    'input:focus ~ &': {
-      border: `1px solid ${theme.palette.primary.main}`,
-    },
-  },
-  checkedIcon: {
-    position: 'relative',
-    width: '1rem',
-    height: '1rem',
-    borderRadius: theme.typography.pxToRem(3),
-    border: `1px solid ${theme.palette.text.primary}`,
-    transition: theme.transitions.create(['border'], {
-      easing: theme.transitions.easing.easeInOut,
-      duration: theme.transitions.duration.short,
-    }),
-    '&::after': {
-      content: '""',
-      display: 'block',
-      position: 'absolute',
-      width: '0.75rem',
-      height: '0.75rem',
-      top: theme.typography.pxToRem(1),
-      left: theme.typography.pxToRem(1),
-      backgroundColor: theme.palette.primary.main,
-      borderRadius: theme.typography.pxToRem(1),
-    },
-    'input:hover ~ &': {
-      border: `1px solid ${theme.palette.primary.main}`,
-    },
-    'input:disabled ~ &': {
-      border: `1px solid ${theme.palette.text.primary}`,
-    },
-    'input:focus ~ &': {
-      border: `1px solid ${theme.palette.primary.main}`,
-    },
-  },
-  checked: {},
-  disabled: {},
-}));
-
+const CheckedIcon = () => (
+  <Box
+    component="span"
+    sx={{
+      position: 'relative',
+      width: '1rem',
+      height: '1rem',
+      borderRadius: theme => theme.typography.pxToRem(3),
+      border: theme => `1px solid ${theme.palette.text.primary}`,
+      transition: theme =>
+        theme.transitions.create(['border'], {
+          easing: theme.transitions.easing.easeInOut,
+          duration: theme.transitions.duration.short,
+        }),
+      '&::after': {
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        width: '0.75rem',
+        height: '0.75rem',
+        top: theme => theme.typography.pxToRem(1),
+        left: theme => theme.typography.pxToRem(1),
+        backgroundColor: theme => theme.palette.primary.main,
+        borderRadius: theme => theme.typography.pxToRem(1),
+      },
+      'input:hover ~ &': {
+        border: theme => `1px solid ${theme.palette.primary.main}`,
+      },
+      'input:disabled ~ &': {
+        border: theme => `1px solid ${theme.palette.text.primary}`,
+      },
+      'input:focus ~ &': {
+        border: theme => `1px solid ${theme.palette.primary.main}`,
+      },
+    }}
+  />
+);
+const Icon = () => (
+  <Box
+    component="span"
+    sx={{
+      width: '1rem',
+      height: '1rem',
+      borderRadius: '0.1875rem',
+      border: theme => `1px solid ${theme.palette.text.primary}`,
+      transition: theme =>
+        theme.transitions.create(['border'], {
+          easing: theme.transitions.easing.easeInOut,
+          duration: theme.transitions.duration.short,
+        }),
+      'input:hover ~ &': {
+        border: theme => `1px solid ${theme.palette.primary.main}`,
+      },
+      'input:disabled ~ &': {
+        border: theme => `1px solid ${theme.palette.text.primary}`,
+      },
+      'input:focus ~ &': {
+        border: theme => `1px solid ${theme.palette.primary.main}`,
+      },
+    }}
+  />
+);
 /**
  * @param {import('@mui/material').CheckboxProps} props
  * @param {React.Ref<any>} ref
  */
 const Checkbox = (props, ref) => {
-  const { icon, checkedIcon, ...checkboxClasses } = checkboxStyles({});
   return (
     <MuiCheckbox
-      classes={checkboxClasses}
-      checkedIcon={<span className={checkedIcon} />}
-      icon={<span className={icon} />}
+      sx={{
+        '&': {
+          padding: '0rem',
+        },
+        '&:hover': {
+          backgroundColor: 'transparent',
+        },
+        '&$checked:hover': {
+          backgroundColor: 'transparent',
+        },
+      }}
+      checkedIcon={<CheckedIcon />}
+      icon={<Icon />}
       inputRef={ref}
       {...props}
     />
