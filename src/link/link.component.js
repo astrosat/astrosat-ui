ab249d0cb6876fed2d5ef0d0ee4ddf22c3c45297
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-05-25 16:18:38 +0100

    fix(frontend): refactor the link CSS code for the new version of Astrosat-ui
    IsseID: 0RB-1137

--- src/link/link.component.js
+++ src/link/link.component.js
@@ -2,29 +2,11 @@ import React from 'react';
 
 import { Link as MuiLink } from '@mui/material';
 
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    borderBottom: `0.0625em solid ${theme.palette.primary.main}`,
-    textDecoration: 'none',
-    '&:hover': {
-      textDecoration: 'none',
-      borderBottomColor: 'transparent',
-    },
-    '&:visited': {
-      color: theme.palette.primary.main,
-    },
-  },
-}));
-
 /**
  * @param {import('@mui/material').LinkProps} props
  * @param {React.Ref<any>} ref
  */
 const Link = (props, ref) => {
-  const classes = useStyles(props);
-
-  return <MuiLink ref={ref} classes={classes} {...props} />;
+  return <MuiLink ref={ref} {...props} />;
 };
 export default React.forwardRef(Link);
