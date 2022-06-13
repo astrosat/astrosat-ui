05ff6dc9ad03dd393d82710d011a82cd334cef33
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-06-13 11:20:04 +0100

    fix(frontend): start rebuilding the password input component

--- src/password-input/password-input.component.js
+++ src/password-input/password-input.component.js
@@ -2,30 +2,31 @@ import React, { useState } from 'react';
 
 import { InputAdornment } from '@mui/material';
 
-import makeStyles from '@mui/styles/makeStyles';
+import { makeStyles, styled } from '@mui/styles';
 
 import { default as IconButton } from '../icon-button/icon-button.component';
 import { default as Input } from '../input/input.component';
 
 import { EyeIcon, EyeSlashIcon } from '../icons';
+import { baseTheme } from '../themes/palette';
 
 const INPUT_TYPE = {
   text: 'text',
   password: 'password',
 };
 
-const iconStyles = makeStyles(theme => ({
-  root: {
-    /**
-     * @param {import('../input/input.component.js').InputProps} props
-     */
-    color: props => {
-      if (props.error) return theme.palette.error.main;
-      if (props.valid) return theme.palette.success.main;
-      return 'inherit';
-    },
-  },
-}));
+// const iconStyles = makeStyles({
+//   root: {
+//     /**
+//      * @param {import('../input/input.component.js').InputProps} props
+//      */
+//     color: props => {
+//       if (props.error) return baseTheme.palette.error.main;
+//       if (props.valid) return baseTheme.palette.success.main;
+//       return 'inherit';
+//     },
+//   },
+// });
 
 /**
  * @param {import('../input/input.component.js').InputProps & {visibilityToggleButtonLabel?: string}} props
@@ -37,7 +38,8 @@ const PasswordInput = ({
 }) => {
   const [_type, setType] = useState(type);
 
-  const iconClasses = iconStyles({ type: _type, ...rest });
+  // const iconClasses = iconStyles({ type: _type, ...rest });
+  // console.log('iconClasses', iconClasses);
 
   const handleClick = () =>
     _type === INPUT_TYPE.password
@@ -52,9 +54,27 @@ const PasswordInput = ({
         aria-label={visibilityToggleButtonLabel}
       >
         {_type === INPUT_TYPE.text ? (
-          <EyeIcon classes={iconClasses} />
+          <EyeIcon
+            sx={{
+              color: (props, theme) => {
+                if (props.error) return theme.palette.error.main;
+                if (props.valid) return theme.palette.success.main;
+                return 'inherit';
+              },
+            }}
+            // classes={iconClasses}
+          />
         ) : (
-          <EyeSlashIcon classes={iconClasses} />
+          <EyeSlashIcon
+            sx={{
+              color: (props, theme) => {
+                if (props.error) return theme.palette.error.main;
+                if (props.valid) return theme.palette.success.main;
+                return 'inherit';
+              },
+            }}
+            // classes={iconClasses}
+          />
         )}
       </IconButton>
     </InputAdornment>
