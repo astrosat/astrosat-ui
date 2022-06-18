efb9bb44262305df6cece9a109283254671bb965
Mark Small <mark.small@astrosat.net>
2022-06-18 01:21:30 +0100

    fix(frontend): Update eslint rollup plugin
    I removed the eslint downgrading for eslint in the `resolutions`
    block of the package.json file. I then swapped the existing eslint
    rollup plugin, for it's more modern version. At some point we should
    look at the others, they possible could/should also be converted over.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // Rollup plugins
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { eslint } from 'rollup-plugin-eslint';
+import eslint from '@rollup/plugin-eslint';
 import includePaths from 'rollup-plugin-includepaths';
 import resolve from '@rollup/plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
